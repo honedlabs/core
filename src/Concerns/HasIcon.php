3b0dedbe93e32f9df5af0f2d3bdd96e4ab02aca0
jdw5 <joshuadwallace1@gmail.com>
2025-02-02 11:56:34 +0000

    chore: updated github action

--- src/Concerns/HasIcon.php
+++ src/Concerns/HasIcon.php
@@ -8,22 +8,17 @@ use Honed\Core\Contracts\IsIcon;
 
 trait HasIcon
 {
-    use EvaluatesClosures {
-        evaluateModelForTrait as evaluateModelForIcon;
-    }
-
     /**
-     * @var string|\Honed\Core\Contracts\IsIcon
+     * @var string|\Honed\Core\Contracts\IsIcon|\Closure|null
      */
     protected $icon;
 
     /**
      * Set the icon for the instance.
      *
-     * @param  string|\Honed\Core\Contracts\IsIcon|null  $icon
      * @return $this
      */
-    public function icon($icon): static
+    public function icon(string|IsIcon|\Closure|null $icon): static
     {
         if (! \is_null($icon)) {
             $this->icon = $icon;
@@ -33,18 +28,27 @@ trait HasIcon
     }
 
     /**
-     * Get the icon for the instance, evaluating it if necessary.
+     * Get the icon for the instance.
+     */
+    public function getIcon(): ?string
+    {
+        return match (true) {
+            $this->icon instanceof IsIcon => $this->icon->icon(),
+            $this->icon instanceof \Closure => $this->resolveIcon(),
+            default => $this->icon,
+        };
+    }
+
+    /**
+     * Evaluate the icon for the instance.
      *
-     * @param  array<string,mixed>|\Illuminate\Database\Eloquent\Model  $parameters
+     * @param  array<string,mixed> $parameters
      * @param  array<string,mixed>  $typed
      */
-    public function getIcon($parameters = [], $typed = []): ?string
+    public function resolveIcon(array $parameters = [], array $typed = []): ?string
     {
-        $evaluated = match (true) {
-            $this->icon instanceof IsIcon => $this->icon->icon(),
-            $parameters instanceof \Illuminate\Database\Eloquent\Model => $this->evaluateModelForIcon($parameters, 'getIcon'),
-            default => $this->evaluate($this->icon, $parameters, $typed),
-        };
+        /** @var string|null */
+        $evaluated = $this->evaluate($this->icon, $parameters, $typed);
 
         $this->icon = $evaluated;
 
@@ -56,6 +60,6 @@ trait HasIcon
      */
     public function hasIcon(): bool
     {
-        return isset($this->icon);
+        return ! \is_null($this->icon);
     }
 }
